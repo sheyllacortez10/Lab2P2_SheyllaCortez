e82fd8901e59d9884d4937fda36a3f017f5d6157
unknown <sheylla10>
2024-01-26 16:03:51 -0600

    7

--- Lab2P2_SheyllaCortez/src/lab2p2_sheyllacortez/Lab2P2_SheyllaCortez.java
+++ Lab2P2_SheyllaCortez/src/lab2p2_sheyllacortez/Lab2P2_SheyllaCortez.java
@@ -75,7 +75,42 @@ public class Lab2P2_SheyllaCortez {
                 }
                 break;
             case "profesor":
-
+                int op = 0;
+                while (op != 2) {
+                    System.out.println("Que desea hacer: ");
+                    System.out.println("0) Listar ");
+                    System.out.println("1) Crear ");
+                    System.out.println("2) Salir ");
+                    op = entrada.nextInt();
+                    switch (op) {
+                        case 0:
+                            listarResources(recursos);
+                            break;
+                        case 1:
+                            int option = 0;
+                            while (option != 6) {
+                                System.out.println("Que tipo de recurso desea crear: ");
+                                System.out.println("1. Libro ");
+                                System.out.println("2. Articulo ");
+                                System.out.println("3. Conferencia ");
+                                System.out.println("4. Cursos en linea ");
+                                System.out.println("5. Conferencias ");
+                                System.out.println("6. Salir");
+                                option = entrada.nextInt();
+                                switch (option) {
+                                    case 1:
+                                        agregarLibro(recursos);
+                                        break;
+                                    case 2:
+                                        break;
+                                    default:
+                                        throw new AssertionError();
+                                }
+                            }
+                        default:
+                            throw new AssertionError();
+                    }
+                }
                 break;
             case "bibliotecario":
 
@@ -84,11 +119,29 @@ public class Lab2P2_SheyllaCortez {
                 System.out.println("Tipo de usuario invalido ");
         }
 
-
     }
     
-    //Método para crear recursos
-    
+    //Método para crear libro
+       public static void agregarLibro(ArrayList recursos) {
+        System.out.println("Crear recursos");
+
+        System.out.println("Ingrese titlo: ");
+        entrada.nextLine();
+        String titulo = entrada.nextLine();
+        System.out.println("Ingrese autor: ");
+        String autor = entrada.nextLine();
+        System.out.println("Ingrese genero: ");
+        String genero = entrada.nextLine();
+        System.out.println("Ingrese ano de publicacion: ");
+        String anoPublic = entrada.nextLine();
+        System.out.println("Ingrese disponibilidad: ");
+        String disponibilidad = entrada.nextLine();
+      
+        Libro mybook = new Libro(titulo, autor, genero, anoPublic, disponibilidad);
+        recursos.add(mybook);
+        
+    }
+
     
     //Método para listar recursos
     public static void listarResources(ArrayList recursos) {
