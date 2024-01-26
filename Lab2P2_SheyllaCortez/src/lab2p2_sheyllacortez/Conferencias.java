3bd04785bc5c64054bcdb1b2beec8f4af71247a4
unknown <sheylla10>
2024-01-26 15:06:53 -0600

    5

--- Lab2P2_SheyllaCortez/src/lab2p2_sheyllacortez/Conferencias.java
+++ Lab2P2_SheyllaCortez/src/lab2p2_sheyllacortez/Conferencias.java
@@ -1,5 +1,7 @@
 package lab2p2_sheyllacortez;
 
+import java.util.Date;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -13,4 +15,18 @@ package lab2p2_sheyllacortez;
 public class Conferencias {
     //Atributos
     private String titulo = "";
+    private String conferencista = "";
+    private Date fecha;
+    private int duracion = 0;
+    private String linkAcces = "";
+    
+    //Constructor
+    public Conferencias(String titulo, String conferencista, Date fecha, int duracion, String li) {
+        this.titulo = titulo;
+        this.conferencista = conferencista;
+        this.fecha = fecha;
+        this.duracion = duracion;
+        this.linkAcces = linkAcces;
+    }
+    
 }
